f9a60ab59c7ad90138963887d5637b3a008b4ff2
Luke Harrison <luketheduke.harrison@gmail.com>
2022-11-08 20:23:08 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // https://www.youtube.com/watch?v=M421fR3TYxg
+
+function scrolldown() {
+	let Body = document.querySelector('body');
+	Body.className = 'scrolled';
+}
+
+function scrollup() {
+	let Body = document.querySelector('body');
+	Body.className = '.';
+}
+
+window.onload = scrollup();
